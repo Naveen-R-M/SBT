8127ceac5460ced55a5e81c23d68640ec9a8925f
Naveen-R-M <naveen.rm.551@gmail.com>
2020-09-04 10:24:06 +0530

    commit 0.1.3

--- lib/my_colors.dart
+++ lib/my_colors.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 class MyColors{
   static Color TEXT_COLOR = Color(0xff8B2A50);
   static Color TEXT_FIELD_BCK = Color(0xffFAC6CD);
+  static Color APP_BCK = Color(0xffF7929E);
 }
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,31 +1,75 @@
 import 'package:SBT/my_colors.dart';
-import 'package:SBT/screens/authentication/auth.dart';
-import 'package:SBT/screens/authentication/login.dart';
+import 'package:SBT/screens/home/thumnail_images.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import 'image_carousel.dart';
 
 class HomeScreen extends StatelessWidget {
   FirebaseUser user;
   HomeScreen({this.user});
-  AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: null,
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: MyColors.TEXT_FIELD_BCK,
-        child: FlatButton(
-          child: Text('Logout'),
-          onPressed: () {
-            _authService.signOut();
-            Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => Login()));
-          },
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: MyColors.APP_BCK,
+        accentColor: MyColors.TEXT_FIELD_BCK,
       ),
+      home: Scaffold(
+        appBar: null,
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: MyColors.APP_BCK,
+          child: Stack(
+            children: [
+              ImageCarousel(collection: 'Carousel', document: 'HomePage',),
+              SingleChildScrollView(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    top:MediaQuery.of(context).size.height / 3
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                    )
+                    ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                          top: 25,
+                          left: 25,
+                        ),
+                        child: Text(
+                          'Categories',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: MyColors.TEXT_COLOR,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 2
+                          ),
+                        ),
+                      ),
+
+                      LoadImages(val: 'Images'),
+                    ],
+                  ),
+                  ),
+              ),
+            ],
+          ),
+          ),
+  ),
     );
-  }
+}
 }
